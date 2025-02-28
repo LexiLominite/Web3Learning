082b25f85b381ca7434c82d545848d2f7b549722
LexiLominite <lalithkumar01010@gmail.com>
2025-02-28 21:14:48 +0530

    2025-02-28 21:14:48

--- Week 0/Assignments/assignment1.js
+++ Week 0/Assignments/assignment1.js
@@ -1,1 +1,20 @@
 // Give an input string that outputs a hash which starts with 00000
+const crypto = require('crypto');
+function findHashwithPrefix(prefix_str){
+
+    let input = 0;
+    while (1)
+    {
+        checkStr = input.toString();
+        let hash = crypto.createHash('sha256').update(checkStr).digest('hex');
+        if (hash.startsWith(prefix_str)){
+            return{input: checkStr, hash: hash}
+            break;
+
+        }
+        input ++;
+    }
+}
+
+const result = findHashwithPrefix('00000');
+console.log(`Input: ${result.input}, Hash: ${result.hash} `)
